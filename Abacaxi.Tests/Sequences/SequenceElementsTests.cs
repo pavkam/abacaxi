7ae681dbcf5e592426cca064ed59721e83fcdbd4
Alexandru Ciobanu <alex+git@ciobanu.org>
2017-04-18 11:56:57 -0300

    Sum

---
+++ Abacaxi.Tests/Sequences/SequenceElementsTests.cs
@@ -0,0 +1,83 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Abacaxi.Tests.Sequences
+{
+    using System;
+    using System.Collections.Generic;
+    using Abacaxi.Sequences;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SequenceElementsTests
+    {
+        private int StdAgg(int a, int b)
+        {
+            return a + b;
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ThrowsException_IfSequenceIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                SequenceElements.FindBiggestSumOfNumberOfElements((int[])null, 1, StdAgg, Comparer<int>.Default));
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ThrowsException_IfCountIsLessThanOne()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { }, 0, StdAgg, Comparer<int>.Default));
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ThrowsException_IfAggregatorIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { 1 }, 1, null, Comparer<int>.Default));
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ThrowsException_IfComparerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { 1 }, 1, StdAgg, null));
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ReturnsBiggestElement_ForCountOfOne()
+        {
+            TestHelper.AssertSequence(
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { 1, 2, 3 }, 1, StdAgg, Comparer<int>.Default),
+                3);
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ReturnsAllElements_ForCountOfThree()
+        {
+            TestHelper.AssertSequence(
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { 3, 2, 1 }, 3, StdAgg, Comparer<int>.Default),
+                1, 2, 3);
+        }
+
+        [Test]
+        public void FindBiggestSumOfNumberOfElements_ReturnsLastThreeElements()
+        {
+            TestHelper.AssertSequence(
+                SequenceElements.FindBiggestSumOfNumberOfElements(new int[] { 100, 200, 1, 50, 70, 188 }, 3, StdAgg, Comparer<int>.Default),
+                100, 188, 200);
+        }
+    }
+}
---
+++ Abacaxi/Sequences/SequenceElements.cs
@@ -0,0 +1,53 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Abacaxi.Sequences
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class implements a number of algorithms that all use sorting to prepare the sequence before working.
+    /// </summary>
+    public static class SequenceElements
+    {
+        /// <summary>
+        /// Finds the elements, which summed, yield the biggest sum.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the <paramref name="sequence"/>.</typeparam>
+        /// <param name="sequence">The sequence of elements.</param>
+        /// <param name="count">The count of elements to consider.</param>
+        /// <param name="aggregateFunc">The aggregate function which sums elements.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <returns>An array of elements with the highest sum.</returns>
+        public static T[] FindBiggestSumOfNumberOfElements<T>(T[] sequence, int count, Func<T, T, T> aggregateFunc, IComparer<T> comparer)
+        {
+            Validate.ArgumentNotNull(nameof(sequence), sequence);
+            Validate.ArgumentNotNull(nameof(aggregateFunc), aggregateFunc);
+            Validate.ArgumentNotNull(nameof(comparer), comparer);
+            Validate.ArgumentGreaterThanOrEqualTo(nameof(count), count, 1);
+            Validate.ArgumentLessThanOrEqualTo(nameof(count), count, sequence.Length);
+
+            var array = new T[sequence.Length];
+            Array.Copy(sequence, array, array.Length);
+            QuickSort.Sort(array, 0, array.Length, comparer);
+
+            var result = new T[count];
+            Array.Copy(array, array.Length - count, result, 0, count);
+
+            return result; 
+        }
+    }
+}
