ffca42cd87e67b67054319f6b57543ed3d9043f7
Alexandru Ciobanu <alex@ciobanu.org>
2017-05-05 18:04:46 -0300

    Word ladder

---
+++ Abacaxi.Practice/Graphs/StringNeighborhoodGraph.cs
@@ -0,0 +1,127 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Abacaxi.Practice.Graphs
+{
+    using System;
+    using System.Collections.Generic;
+    using Abacaxi.Graphs;
+
+    /// <summary>
+    /// A graph composed by a number of strings (representing vertices) and connected by edges signifying potential one-letter transformations.
+    /// </summary>
+    public sealed class StringNeighborhoodGraph : Graph<string>
+    {
+        private readonly IDictionary<string, ISet<string>> _neighborhoods;
+        private readonly ISet<string> _vertices;
+
+        private static IEnumerable<string> GetAllStringPatterns(string s)
+        {
+            var ca = (s ?? string.Empty).ToCharArray();
+            var result = new string[ca.Length];
+            for (var i = 0; i < ca.Length; i++)
+            {
+                var temp = ca[i];
+                ca[i] = '\0';
+                result[i] = new string(ca);
+                ca[i] = temp;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this graph's edges are directed.
+        /// </summary>
+        /// <value>
+        /// Always returns <c>false</c>.
+        /// </value>
+        public override bool IsDirected => false;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is read only.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is read only; otherwise, <c>false</c>.
+        /// </value>
+        public override bool IsReadOnly => true;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringNeighborhoodGraph"/> class.
+        /// </summary>
+        /// <param name="sequence">The sequence of strings to build the graph upon.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="sequence"/> is <c>null</c>.</exception>
+        public StringNeighborhoodGraph(IEnumerable<string> sequence)
+        {
+            Validate.ArgumentNotNull(nameof(sequence), sequence);
+
+            _neighborhoods = new Dictionary<string, ISet<string>>();
+            _vertices = new HashSet<string>();
+            foreach (var item in sequence)
+            {
+                _vertices.Add(item);
+
+                foreach (var pattern in GetAllStringPatterns(item))
+                {
+                    if (!_neighborhoods.TryGetValue(pattern, out ISet<string> neighbors))
+                    {
+                        neighbors = new HashSet<string>();
+                        _neighborhoods.Add(pattern, neighbors);
+                    }
+
+                    neighbors.Add(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates all vertices in the graph.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        public override IEnumerator<string> GetEnumerator()
+        {
+            return _vertices.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the edges for a given <param name="vertex" />.
+        /// </summary>
+        /// <param name="vertex"></param>
+        /// <returns>
+        /// A sequence of edges connected to the given <param name="vertex" />
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown if the <paramref name="vertex"/> is not part of the graph.</exception>
+        public override IEnumerable<Edge<string>> GetEdges(string vertex)
+        {
+            if (vertex == null || !_vertices.Contains(vertex))
+            {
+                throw new ArgumentException($"Vertex '{vertex}' is not part of this graph.", nameof(vertex));
+            }
+
+            foreach (var pattern in GetAllStringPatterns(vertex))
+            {
+                foreach (var neighbor in _neighborhoods[pattern])
+                {
+                    if (neighbor != vertex)
+                    {
+                        yield return new Edge<string>(vertex, neighbor);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Abacaxi.Tests/Graph/StringNeighborhoodGraphTests.cs
@@ -0,0 +1,103 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+// ReSharper disable ObjectCreationAsStatement
+
+namespace Abacaxi.Tests.Graph
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+    using Practice.Graphs;
+
+    [TestFixture]
+    public class StringNeighborhoodGraphTests
+    {
+        [Test]
+        public void Ctor_ThrowsException_ForNullSequence()
+        {
+            Assert.Throws<ArgumentNullException>(() => new StringNeighborhoodGraph(null));
+        }
+
+        [Test]
+        public void Enumeration_ReturnsNothing_ForEmptyGraph()
+        {
+            var graph = new StringNeighborhoodGraph(new string[] {});
+
+            TestHelper.AssertSequence(graph);
+        }
+
+        [Test]
+        public void Enumeration_ReturnsSingleVertex()
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "test" });
+
+            TestHelper.AssertSequence(graph, "test");
+        }
+
+        [Test]
+        public void Enumeration_ReturnsAllVertices()
+        {
+            var graph = new StringNeighborhoodGraph(new [] { "a", "b", "c" });
+
+            TestHelper.AssertSequence(graph, "a", "b", "c");
+        }
+
+        [Test]
+        public void GetEdges_ThrowsException_ForNullVertex()
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "a", "b", "c" });
+
+            Assert.Throws<ArgumentException>(() => graph.GetEdges(null).ToList());
+        }
+
+        [Test]
+        public void GetEdges_ThrowsException_ForInvalidVertex()
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "a", "b", "c" });
+
+            Assert.Throws<ArgumentException>(() => graph.GetEdges("z").ToList());
+        }
+
+        [Test]
+        public void GetEdges_CorrectlyReports_FromVertex()
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "a", "b" });
+
+            Assert.AreEqual("a", graph.GetEdges("a").Single().FromVertex);
+        }
+
+        [TestCase("a", "b,c")]
+        [TestCase("b", "a,c")]
+        [TestCase("c", "a,b")]
+        public void GetEdges_ProperlyJoinsSingleLetterStrings(string vertex, string expected)
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "a", "b", "c" });
+            var actual = string.Join(",", graph.GetEdges(vertex).Select(s => s.ToVertex));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FindShortestPath_WillTraverseTheGraphInExpectedOrder()
+        {
+            var graph = new StringNeighborhoodGraph(new[] { "aa", "za", "zz", "zb", "ib", "i7", "17", "a6", "16" });
+            
+            TestHelper.AssertSequence(graph.FindShortestPath("aa", "17"),
+                "aa", "a6", "16", "17");
+        }
+    }
+}
