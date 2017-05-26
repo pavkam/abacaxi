95a6dc1fc71fb2017337e78163a505d22a1eb6f9
Alexandru Ciobanu <alex@ciobanu.org>
2017-05-26 04:35:02 -0300

    Temporary

---
+++ Abacaxi.Tests/Containers/TemporaryTests.cs
@@ -0,0 +1,91 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+using System.Threading;
+
+namespace Abacaxi.Tests.Containers
+{
+    using System;
+    using Abacaxi.Containers;
+    using NUnit.Framework;
+    using System.Diagnostics.CodeAnalysis;
+
+    [TestFixture]
+    [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+    public class TemporaryTests
+    {
+        [Test]
+        public void Ctor_ThrowsException_WhenValueFuncIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Temporary<int>(null, 1));
+        }
+
+        [Test]
+        public void Ctor_ThrowsException_WhenValueLifespanMillisIsLessThanOne()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Temporary<int>(() => 1, 0));
+        }
+
+        [Test]
+        public void Temporary_DoesNotInitializeTheValueImmediatelly()
+        {
+            var called = false;
+            new Temporary<int>(() =>
+            {
+                called = true;
+                return -1;
+            }, 1);
+
+            Assert.IsFalse(called);
+        }
+
+        [Test]
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        public void Temporary_InitializesTheValueOnFirstCall()
+        {
+            var called = false;
+            var temp = new Temporary<int>(() =>
+            {
+                called = true;
+                return -1;
+            }, 1);
+
+            var x = temp.Value;
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        [SuppressMessage("ReSharper", "RedundantAssignment")]
+        [SuppressMessage("ReSharper", "NotAccessedVariable")]
+        public void Temporary_ResetsTheValueOfExpiredResource()
+        {
+            var called = 0;
+            var temp = new Temporary<int>(() =>
+            {
+                called++;
+                return -1;
+            }, 1);
+
+            var x = temp.Value;
+            Thread.Sleep(10);
+
+            x = temp.Value;
+            Assert.IsTrue(called > 1);
+        }
+    }
+}
---
+++ Abacaxi/Containers/Temporary.cs
@@ -0,0 +1,80 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Abacaxi.Containers
+{
+    using System;
+
+    /// <summary>
+    /// Class that hold a temporary value for a give resource.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public sealed class Temporary<T>
+    {
+        private readonly object _lock = new object();
+        private readonly Func<T> _valueFunc;
+        private readonly int _valueTtl;
+
+        private T _value;
+        private DateTime _valueExpiryDateTime;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Temporary{T}"/> class.
+        /// </summary>
+        /// <param name="valueFunc">The function that creates a new value.</param>
+        /// <param name="valueLifespanMillis">The lifespan of the created resource (in milliseconds).</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="valueFunc"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="valueLifespanMillis"/> is less than one.</exception>
+        public Temporary(Func<T> valueFunc, int valueLifespanMillis)
+        {
+            Validate.ArgumentNotNull(nameof(valueFunc), valueFunc);
+            Validate.ArgumentGreaterThanZero(nameof(valueLifespanMillis), valueLifespanMillis);
+
+            _valueTtl = valueLifespanMillis;
+            _valueExpiryDateTime = DateTime.MinValue;
+            _valueFunc = valueFunc;
+        }
+
+        /// <summary>
+        /// Gets the resource value. This property is thread-safe and might be blocking (during the duration of resource refresh).
+        /// </summary>
+        /// <value>
+        /// The resource value.
+        /// </value>
+        public T Value
+        {
+            get
+            {
+                var now = DateTime.Now;
+                if (_valueExpiryDateTime < now)
+                {
+                    lock (_lock)
+                    {
+                        if (_valueExpiryDateTime < now)
+                        {
+                            _valueExpiryDateTime = now.AddMilliseconds(_valueTtl);
+                            _value = _valueFunc();
+                        }
+                    }
+                }
+
+                return _value;
+            }
+        }
+    }
+}
