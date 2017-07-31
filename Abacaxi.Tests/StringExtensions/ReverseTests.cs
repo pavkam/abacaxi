24094f81f426ff77133754fe93319343fc2b9a24
Alexandru Ciobanu <alex@ciobanu.org>
2017-07-31 14:05:23 -0300

    A valid string reverse algorithm.

---
+++ Abacaxi.Tests/StringExtensions/ReverseTests.cs
@@ -0,0 +1,41 @@
+﻿/* Copyright 2017 by Alexandru Ciobanu (alex+git@ciobanu.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Abacaxi.Tests.StringExtensions
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class ReverseTests
+    {
+        [Test]
+        public void Reverse_ThrowsException_IfStringIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((string)null).Reverse());
+        }
+
+        [TestCase("", "")]
+        [TestCase("1", "1")]
+        [TestCase("Hello World", "dlroW olleH")]
+        [TestCase("Les Misérables", "selbarésiM seL")]
+        [TestCase("This is: 🀜", "🀜 :si sihT")]
+        public void Reverse_ReversesTheString_AsExpected(string input, string expected)
+        {
+            var actual = input.Reverse();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Abacaxi/StringExtensions.cs
+++ Abacaxi/StringExtensions.cs
@@ -18,6 +18,7 @@ namespace Abacaxi
     using System.Collections.Generic;
     using Internal;
     using JetBrains.Annotations;
+    using System.Globalization;
 
     /// <summary>
     /// Extension method for the <see cref="string"/> data type.
@@ -35,6 +36,29 @@ namespace Abacaxi
         {
             Validate.ArgumentNotNull(nameof(s), s);
             return new StringListWrapper(s);
+        }
+
+        /// <summary>
+        /// Reverses the specified string using "undivided" string chunks.
+        /// </summary>
+        /// <param name="s">The string to reverse.</param>
+        /// <returns>The reserved string.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="s"/> is <c>null</c>.</exception>
+        public static string Reverse(this string s)
+        {
+            Validate.ArgumentNotNull(nameof(s), s);
+
+            var result = new char[s.Length];
+            var index = result.Length;
+            var enumerator = StringInfo.GetTextElementEnumerator(s);
+            while (enumerator.MoveNext())
+            {
+                var chunk = (string) enumerator.Current;
+                index -= chunk.Length;
+                chunk.CopyTo(0, result, index, chunk.Length);
+            }
+
+            return new string(result);
         }
     }
 }
